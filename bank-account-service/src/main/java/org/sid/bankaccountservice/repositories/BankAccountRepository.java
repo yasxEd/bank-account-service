46805ab158747582a30567e0e2b8d7675b1f156e
yasxEd <yasseretudiga@gmail.com>
2024-04-28 07:15:16 +0100

    Initial commit

---
+++ bank-account-service/src/main/java/org/sid/bankaccountservice/repositories/BankAccountRepository.java
@@ -0,0 +1,17 @@
+package org.sid.bankaccountservice.repositories;
+
+import org.hibernate.sql.exec.spi.JdbcCallParameterExtractor;
+import org.sid.bankaccountservice.entities.BankAccount;
+import org.sid.bankaccountservice.enums.AccountType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+
+import java.util.List;
+
+@RepositoryRestController
+public interface BankAccountRepository extends JpaRepository<BankAccount, String> {
+    @RestResource(path = "/byType")
+    List<BankAccount> findByType(@Param("t") AccountType type);
+}
